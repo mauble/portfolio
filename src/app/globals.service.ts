f6d3e2ac041c176d6f604b45e2f22b6edf91a047
mauble <92347868+mauble@users.noreply.github.com>
2025-05-16 06:46:54 +0200

    Add GlobalsService

---
+++ src/app/globals.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class GlobalsService {
+  public emailAddress = "mauble@proton.me";
+  public githubUrl = "https://github.com/mauble";
+}

0baf13bd083161a21bdcc0ac870ab6b2a665fa63
mauble <92347868+mauble@users.noreply.github.com>
2025-06-30 14:52:46 +0200

    Add new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { LegalNoticeComponent } from "./legal-notice/legal-notice.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
+  },
+  {
+    path: "legal-notice",
+    component: LegalNoticeComponent,
   },
   {
     path: "**",

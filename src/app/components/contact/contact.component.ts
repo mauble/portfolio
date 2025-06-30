9891eab21dcc6f7b7e62d03a145201212e22cba6
mauble <92347868+mauble@users.noreply.github.com>
2025-06-30 16:04:52 +0200

    Update Contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,29 @@
 import { Component } from "@angular/core";
+import { GlobalsService } from "../../globals.service";
+import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-contact",
-  imports: [],
+  imports: [FormsModule],
   templateUrl: "./contact.component.html",
   styleUrl: "./contact.component.css",
 })
-export class ContactComponent {}
+export class ContactComponent {
+  constructor(public readonly globals: GlobalsService) {}
+
+  form = {
+    email: "",
+    title: "",
+    message: "",
+  };
+
+  onSubmit(contactForm: NgForm) {
+    if (contactForm.valid) {
+      alert(
+        `Email: ${this.form.email}\nTitle: ${this.form.title}\nMessage: ${this.form.message}`,
+      );
+      contactForm.resetForm();
+      this.form = { email: "", title: "", message: "" };
+    }
+  }
+}
